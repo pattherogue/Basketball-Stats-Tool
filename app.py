3d99acd9a7e4aa99e6748f7ab8b268e31d03b44e
Paolo Gomez <peweegardner248@gmail.com>
2023-01-31 10:25:19 -0800

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,8 @@
     #read existing player data from PLAYERS im constants.py
     #clean player data w/o changing original data
     #save new collection
+        #height -- saved as an integer
+        #experience -- saved as a boolean value (True or False)
 #Create balance_teams function
 #Console readability matters
 #Displaying the statsd
