d1dce8734a5a4eb2f510a76d17756df35abff244
Paolo Gomez <peweegardner248@gmail.com>
2023-01-31 10:22:28 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 #Proper use of Dunder Main
 #Import player and team data
+    #import from constants.py
+    #should not be hard coded into script
 #Create a clean_data function
 #Create balance_teams function
 #Console readability matters
-#Displaying the stats
+#Displaying the statsd
