366d11602c9b063dccead90c8d9d1ffceb0e7019
Paolo Gomez <peweegardner248@gmail.com>
2023-01-31 10:23:44 -0800

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,9 @@
     #import from constants.py
     #should not be hard coded into script
     #changes should be reflected
-    
+
 #Create a clean_data function
+    #read existing player data from PLAYERS im constants.py
 #Create balance_teams function
 #Console readability matters
 #Displaying the statsd
