763fe20d62287d28f0d22c6106fd203725425e1c
Paolo Gomez <peweegardner248@gmail.com>
2023-02-01 11:31:26 -0800

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ def clean_data(data):
         fixed = {}
         fixed["email"] = user["email"]
         fixed["first_name"] = user["name"].split(" ")[0]
+    return cleaned
 
     #read existing player data from PLAYERS im constants.py
     #clean player data w/o changing original data
