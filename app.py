e9fb2cda5b003028e91144b04592282b5bdc44e1
Paolo Gomez <peweegardner248@gmail.com>
2023-02-03 11:30:03 -0800

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,10 @@ def clean_data(data):
         else: 
             fixed['experience'] = False
        cleaned = {
-           'name' = player['name']
+           'name': player['name']
+           'gaurdians': 
+           'experience': 
+           'height': 
        }
 
         
