184ddba2212e8bbe3967ca3fd463ddd651072849
Paolo Gomez <peweegardner248@gmail.com>
2023-02-17 12:26:32 -0800

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def clean_data():
     #data returned cleaned
     return cleaned
 
-#Create balance_teams function
+# Create balance_teams function
 def balance_teams():
 # Balance players across three teams: Panthers, Bandits, and  Warriors
     team_panthers = []
