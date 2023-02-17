d6557f3f859bfe520d3b5e0b36b578da2dd76802
Paolo Gomez <peweegardner248@gmail.com>
2023-02-17 12:27:04 -0800

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from constants import TEAMS, PLAYERS
 # Create a clean_data function
 def clean_data():
     # read existing player data from PLAYERS im constants.py
-    #clean player data w/o changing original data
+    # clean player data w/o changing original data
     cleaned = []
     for player in PLAYERS:
         fixed = {}
