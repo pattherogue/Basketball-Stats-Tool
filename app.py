987111a807315cf8203aa1db601ef639ba535331
Paolo Gomez <peweegardner248@gmail.com>
2023-02-22 10:50:28 -0800

    Update app.py

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ while True:
     print("\n")
     while True: 
         # Enter an option: A
-        option = input("Enter an option: ")
+        option = input("Enter an option: A/B")
         if option == 'A':
             print("\n\nA) Panthers \nB) Bandits \nC) Warriors")
             # A) Panthers
